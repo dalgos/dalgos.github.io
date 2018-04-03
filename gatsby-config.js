213ec39ce35f3f039b32361e20d5fd74acb552eb
kwangs <kwangs@tmon.co.kr>
2018-04-03 19:15:30 +0900

    Configure gatsby's setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'oror.io',
   },
   plugins: ['gatsby-plugin-react-helmet'],
+  pathPrefix: '/dalgos.github.io'
 };
