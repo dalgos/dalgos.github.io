1d2c6a79819af0f7dbb3e045ce5876f0f3a1e351
kwangs <kwangs@tmon.co.kr>
2018-04-03 19:04:17 +0900

    Initialize Gatsby 🔥

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/kwangs/Documents/GIT/private/dalgos.github.io/src/layouts/index.js"
+  import data from "/Users/kwangs/Documents/GIT/private/dalgos.github.io/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
