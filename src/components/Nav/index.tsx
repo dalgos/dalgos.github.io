7e1403b771d6dae3a8706d10246b623d50dcd57b
kwangs <kwangs@tmon.co.kr>
2018-06-26 18:19:15 +0900

    feat: Upgrade Gatsby 2

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Link from 'gatsby-link'
+import { Link } from 'gatsby'
 import * as classnames from 'classnames'
 
 export default ({ className, ...restProps }) =>
--- src/components/PostItem/index.tsx
+++ src/components/PostItem/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Link from 'gatsby-link'
+import { Link } from 'gatsby'
 import Styled from 'styled-components'
 import Img from 'gatsby-image'
 
