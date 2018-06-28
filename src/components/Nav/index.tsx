64ceafee395e05edf8d05e3e6b1c968c2fed0b24
dalgos <dalgos@dalgos.com>
2018-06-29 00:28:14 +0900

    feat: Apply typescript

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
 import * as classnames from 'classnames'
 
+import Logo from '../Logo'
+
 export default ({ className, ...restProps }) => (
   <StaticQuery
     query={graphql`
@@ -16,7 +18,12 @@ export default ({ className, ...restProps }) => (
     render={data => (
       <nav className={classnames('db dt-l w-100 border-box pa3 ph5-l near-black', className)}>
         <Link className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" to="/" title="Home">
-          <h1 className="f3 mv0">{data.site.siteMetadata.title}</h1>
+          <h1 className="f3 mv0">
+            <Logo
+              width="1.5rem"
+              height="1.5rem"
+            />
+          </h1>
         </Link>
       </nav>
     )}
--- src/components/PostItem/index.tsx
+++ src/components/PostItem/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Link from 'gatsby-link'
 import Styled from 'styled-components'
 import Img from 'gatsby-image'
 
