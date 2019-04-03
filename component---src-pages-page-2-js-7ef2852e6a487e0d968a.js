c3be58f085a3cf431d294d47ef3067ceb2320488
dalgos <dalgos@dalgos.com>
2019-04-04 02:16:07 +0900

    Updates

---
+++ component---src-pages-page-2-js-7ef2852e6a487e0d968a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{160:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(56);n.default=function(){return o.a.createElement("div",null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-7ef2852e6a487e0d968a.js.map
---
+++ component---src-pages-page-2-js-b60c71cf7c17be08a0e8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{148:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(38);n.default=function(){return o.a.createElement("div",null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-b60c71cf7c17be08a0e8.js.map
