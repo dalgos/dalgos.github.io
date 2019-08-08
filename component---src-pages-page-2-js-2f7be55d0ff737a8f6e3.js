b5d56507c73c3ac2c9fc32719b58583978345e2a
dalgos <dalgos@dalgos.com>
2019-08-09 02:01:15 +0900

    Updates

---
+++ component---src-pages-page-2-js-2f7be55d0ff737a8f6e3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{210:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(93);n.default=function(){return o.a.createElement("div",null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-2f7be55d0ff737a8f6e3.js.map
--- styles-a9af8393e08fe942273a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{165:function(n,w,o){}}]);
-//# sourceMappingURL=styles-a9af8393e08fe942273a.js.map
