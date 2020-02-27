89e954bb16120aca217e566188df033939dbb4a2
dalgos <function.bind@gmail.com>
2020-02-28 01:28:25 +0900

    feat: Configure MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,72 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: 'cwa',
-    author: 'dalgos',
+    author: 'kei',
     meta: [
-      { name: 'description', content: 'dalgos blog' },
-      { name: 'keywords', content: 'frontend, javascript' },
+      { name: 'description', content: 'blog' },
+      { name: 'keywords', content: 'frontend, javascript, typescript' },
     ],
     description: `Advanced Frontend Engineer`
   },
   plugins: [
-    'gatsby-plugin-catch-links',
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/Layout.tsx'),
+        }
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-typescript',
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `pages`,
+    //     path: `${__dirname}/src/pages`,
+    //   }
+    // },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: `images`,
+        path: `${__dirname}/src/images`,
       }
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-prismjs',
-            options: {
-              classPrefix: 'language-',
-              inlineCodeMarker: null,
-              aliases: {},
-            }
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 590,
-            }
-          }
-        ]
-      }
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    // {
+    //   resolve: "gatsby-plugin-page-creator",
+    //   options: {
+    //     path: `${__dirname}/src/posts`,
+    //   },
+    // },
+    // {
+    //   resolve: "gatsby-plugin-page-creator",
+    //   options: {
+    //     path: `${__dirname}/src/pages`,
+    //   },
+    // },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'cwa',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png',
+      },
     },
     {
       resolve: 'gatsby-plugin-sass',
@@ -46,15 +74,44 @@ module.exports = {
         precision: 8,
       }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    // 'gatsby-plugin-catch-links',
+    // {
+    //   resolve: 'gatsby-transformer-remark',
+    //   options: {
+    //     plugins: [
+    //       {
+    //         resolve: 'gatsby-remark-prismjs',
+    //         options: {
+    //           classPrefix: 'language-',
+    //           inlineCodeMarker: null,
+    //           aliases: {},
+    //         }
+    //       },
+    //       {
+    //         resolve: 'gatsby-remark-images',
+    //         options: {
+    //           maxWidth: 590,
+    //         }
+    //       }
+    //     ]
+    //   }
+    // },
+    
+    
+    
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-121406525-1',
         head: false,
       }
-    }
+    },
+    // {
+    //   resolve: `gatsby-plugin-mdx`,
+    //   options: {
+    //     extensions: [`.mdx`, `.md`],
+    //   },
+    // },
   ],
   pathPrefix: '/'
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -6,79 +6,136 @@
 
  // You can delete this file if you're not using it
 
+// const path = require('path')
+// const { createFilePath } = require('gatsby-source-filesystem')
+
+// exports.createPages = async ({ actions, graphql, reporter }) => {
+//   const { createPage } = actions
+//   const blogPostTemplate = path.resolve('src/templates/blog-post.tsx')
+
+//   const result = await graphql(`{
+//     allMarkdownRemark(
+//       sort: { order: DESC, fields: [frontmatter___date] }
+//       limit: 1000
+//     ) {
+//       edges {
+//         node {
+//           excerpt(pruneLength: 250)
+//           html
+//           id
+//           fields {
+//             slug
+//           }
+//           frontmatter {
+//             date
+//             path
+//             title
+//           }
+//         }
+//       }
+//     }
+//   }`)
+
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`)
+//     return
+//   }
+//   const posts = result.data.allMarkdownRemark.edges
+//   posts.forEach(({ node }, index) => {
+//     const previous = index === posts.length - 1 ? null : posts[index + 1].node
+//     const next = index === 0 ? null : posts[index - 1].node
+//     createPage({
+//       path: node.frontmatter.path,
+//       component: blogPostTemplate,
+//       context: {
+//         slug: node.fields.slug,
+//         previous,
+//         next,
+//       }
+//     })
+//   })
+//     // Create blog-list pages
+//   const postsPerPage = 5
+//   const numPages = Math.ceil(posts.length / postsPerPage)
+//   Array.from({ length: numPages }).forEach((_, idx) => {
+//     createPage({
+//       path: `${idx === 0 ? '/' : '/blog/' + (idx + 1)}`,
+//       component: path.resolve('./src/templates/blog-list.tsx'),
+//       context: {
+//         limit: postsPerPage,
+//         skip: idx * postsPerPage,
+//         numPages,
+//         currentPage: idx + 1,
+//       }
+//     })
+//   })
+// }
+
+// exports.onCreateNode = ({ node, getNode, actions }) => {
+//   const { createNodeField } = actions
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const slug = createFilePath({ node, getNode, basePath: `pages` })
+//     createNodeField({
+//       node,
+//       name: `slug`,
+//       value: slug,
+//     })
+//   }
+// }
+
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions
-  const blogPostTemplate = path.resolve('src/templates/blog-post.tsx')
+exports.onCreateNode = ({ node, actions, getNode, ...rest }, options) => {
 
-  const result = await graphql(`{
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] }
-      limit: 1000
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 250)
-          html
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            date
-            path
-            title
+  const { createNodeField } = actions
+
+  if (node.internal.type === 'Mdx') {
+    // const fileNode = getNode(node.parent)
+    // const source = fileNode.sourceInstanceName
+    const value = createFilePath({ node, getNode })
+    // console.log(source, value)
+    createNodeField({
+      // 추가하는 필드명
+      name: 'slug',
+      // MDX Node
+      node,
+      // 'post' prefix를 기반으로 포스트를 작성한다.
+      value: `${node.frontmatter.path}`,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            frontmatter {
+              path
+              title
+            }
+            fileAbsolutePath
           }
         }
       }
     }
-  }`)
-
+  `)
   if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
   }
-  const posts = result.data.allMarkdownRemark.edges
-  posts.forEach(({ node }, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
+  // post page를 생성합니다.
+  const posts = result.data.allMdx.edges
+  posts.forEach(({ node: { frontmatter, id, fileAbsolutePath } }, idx) => {
+    console.log(frontmatter.path)
     createPage({
-      path: node.frontmatter.path,
-      component: blogPostTemplate,
-      context: {
-        slug: node.fields.slug,
-        previous,
-        next,
-      }
-    })
-  })
-    // Create blog-list pages
-  const postsPerPage = 5
-  const numPages = Math.ceil(posts.length / postsPerPage)
-  Array.from({ length: numPages }).forEach((_, idx) => {
-    createPage({
-      path: `${idx === 0 ? '/' : '/blog/' + (idx + 1)}`,
-      component: path.resolve('./src/templates/blog-list.tsx'),
-      context: {
-        limit: postsPerPage,
-        skip: idx * postsPerPage,
-        numPages,
-        currentPage: idx + 1,
-      }
+      path: frontmatter.path,
+      component: fileAbsolutePath,
+      context: { id },
     })
   })
 }
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
-}
